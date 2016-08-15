6dcb0d5e9cf732dc9076841037d40048ec69fb76
Álex Puchades <alex94puchades@gmail.com>
2016-08-15 05:10:27 +0200

    Simplify parser internals

--- rejson/parse.hpp
+++ rejson/parse.hpp
@@ -30,34 +30,31 @@ REJSON_EXPORT Value parse(detail::u32string_view sv);
 template <class Iterator>
 Value parse(Iterator begin, Iterator end);
 
-template <typename CharT>
-Value parse(std::basic_istream<CharT> & is);
+template <typename Char>
+Value parse(std::basic_istream<Char> & is);
 
 namespace detail {
 
 template <class Iterator>
-Value parse_value(Iterator begin, Iterator end, Iterator & iter);
-
-void encode_utf8(char32_t code_pt, std::ostringstream & os);
+Value parse_value(Iterator & begin, Iterator end);
 
 template <class Iterator>
 using char_type = typename std::iterator_traits<Iterator>::value_type;
 
-template <class Iterator, typename CharT>
-bool try_next_char(Iterator begin, Iterator end, Iterator & iter, CharT & chr)
+template <class Iterator, typename Char>
+bool try_next_char(Iterator & begin, Iterator end, Char & chr)
 {
 	if (begin == end)
 		return false;
-	iter = begin;
-	chr = *iter++;
+	chr = *begin++;
 	return true;
 }
 
 template <class Iterator>
-auto next_char(Iterator begin, Iterator end, Iterator & iter)
+auto next_char(Iterator & begin, Iterator end)
 {
 	char_type<Iterator> chr;
-	if (!try_next_char(begin, end, iter, chr))
+	if (!try_next_char(begin, end, chr))
 		throw ParseError("unexpected end of input");
 	return chr;
 }
@@ -70,84 +67,75 @@ char_type<Iterator> peek_char(Iterator begin, Iterator end)
 	return *begin;
 }
 
-template <class Iterator, typename CharT>
-void consume(Iterator begin, Iterator end, CharT token, Iterator & iter)
+template <class Iterator, typename Char>
+void consume(Iterator & begin, Iterator end, Char token)
 {
 	using namespace std::literals::string_literals;
-	const auto chr = next_char(begin, end, iter);
+	const auto chr = peek_char(begin, end);
 	if (chr != token)
 		throw ParseError("expected '"s + token + "' token");
+	++begin;
 }
 
-template <class Iterator, typename CharT>
-bool try_consume(Iterator begin, Iterator end, CharT token, Iterator & iter)
+template <class Iterator, typename Char>
+bool try_consume(Iterator & begin, Iterator end, Char token)
 {
-	Iterator try_iter;
-	char_type<Iterator> chr;
-	if (!try_next_char(begin, end, try_iter, chr))
-		return false;
-	if (chr != token)
-		return false;
-	iter = try_iter;
-	return true;
+	if (begin != end && *begin == token)
+		return ++begin, true;
+	return false;
 }
 
-template <class Iterator, typename CharT>
-bool try_consume(Iterator begin, Iterator end,
-                 const CharT * tokens, Iterator & iter)
+template <class Iterator, typename Char>
+bool try_consume(Iterator & begin, Iterator end, const Char * tokens)
 {
 	Iterator try_iter = begin;
-	while (const char t = *tokens++) {
-		char_type<Iterator> chr;
-		if (!try_next_char(try_iter, end, try_iter, chr))
-			return false;
-		if (chr != t)
+	while (const auto t = *tokens++) {
+		if (!try_consume(try_iter, end, t))
 			return false;
 	}
-	iter = try_iter;
+	begin = try_iter;
 	return true;
 }
 
 template <class Iterator>
-void skip_whitespace(Iterator begin, Iterator end, Iterator & iter)
+void skip_whitespace(Iterator & begin, Iterator end)
 {
-	for (iter = begin; iter != end; ++iter) {
-		if (!std::isspace(*iter))
-			return;
+	for (; begin != end; ++begin) {
+		if (!std::isspace(*begin))
+			break;
 	}
 }
 
 template <class Iterator>
-Null parse_null(Iterator begin, Iterator end, Iterator & iter)
+Null parse_null(Iterator & begin, Iterator end)
 {
-	if (!try_consume(begin, end, "null", iter))
+	if (!try_consume(begin, end, "null"))
 		throw ParseError("invalid value");
 	return nullptr;
 }
 
 template <class Iterator>
-Bool parse_true(Iterator begin, Iterator end, Iterator & iter)
+Bool parse_true(Iterator & begin, Iterator end)
 {
-	if (!try_consume(begin, end, "true", iter))
+	if (!try_consume(begin, end, "true"))
 		throw ParseError("invalid value");
 	return true;
 }
 
 template <class Iterator>
-Bool parse_false(Iterator begin, Iterator end, Iterator & iter)
+Bool parse_false(Iterator & begin, Iterator end)
 {
-	if (!try_consume(begin, end, "false", iter))
+	if (!try_consume(begin, end, "false"))
 		throw ParseError("invalid value");
 	return false;
 }
 
 template <class Iterator>
-bool try_parse_xdigit(Iterator begin, Iterator end,
-                      Iterator & iter, std::uint8_t & xdigit)
+bool try_parse_xdigit(Iterator & begin, Iterator end, std::uint8_t & xdigit)
 {
-	Iterator try_iter;
 	char_type<Iterator> chr;
-	if (!try_next_char(begin, end, try_iter, chr))
+	Iterator try_iter = begin;
+	if (!try_next_char(try_iter, end, chr))
 		return false;
 	if (std::isdigit(chr)) {
 		xdigit = chr - '0';
@@ -158,27 +146,26 @@ bool try_parse_xdigit(Iterator begin, Iterator end,
 	} else {
 		return false;
 	}
-	iter = try_iter;
+	begin = try_iter;
 	return true;
 }
 
 template <class Iterator>
-bool try_parse_codept(Iterator begin, Iterator end,
-                      Iterator & iter, char16_t & code_pt)
+bool try_parse_codept(Iterator & begin, Iterator end, char16_t & code_pt)
 {
-	Iterator try_iter;
+	Iterator try_iter = begin;
 	std::uint8_t d0, d1, d2, d3;
-	if (!try_consume(begin, end, "\\u", try_iter)
-	    || !try_parse_xdigit(try_iter, end, try_iter, d0)
-	    || !try_parse_xdigit(try_iter, end, try_iter, d1)
-	    || !try_parse_xdigit(try_iter, end, try_iter, d2)
-	    || !try_parse_xdigit(try_iter, end, try_iter, d3))
+	if (!try_consume(try_iter, end, "\\u")
+	    || !try_parse_xdigit(try_iter, end, d0)
+	    || !try_parse_xdigit(try_iter, end, d1)
+	    || !try_parse_xdigit(try_iter, end, d2)
+	    || !try_parse_xdigit(try_iter, end, d3))
 		return false;
 	code_pt = d0 << ((CHAR_BIT / 2) * 3)
 	        | d1 << ((CHAR_BIT / 2) * 2)
 	        | d2 << ((CHAR_BIT / 2) * 1)
 	        | d3 << ((CHAR_BIT / 2) * 0);
-	iter = try_iter;
+	begin = try_iter;
 	return true;
 }
 
@@ -195,10 +182,10 @@ constexpr bool is_codept_group(char16_t first, char16_t second)
 }
 
 template <class Iterator>
-char parse_escaped(Iterator begin, Iterator end, Iterator & iter)
+char_type<Iterator> parse_escaped(Iterator & begin, Iterator end)
 {
-	consume(begin, end, '\\', iter);
-	const auto chr = next_char(iter, end, iter);
+	consume(begin, end, '\\');
+	const auto chr = next_char(begin, end);
 	switch (chr) {
 	case 'b': return '\b';
 	case 'f': return '\f';
@@ -209,21 +196,40 @@ char parse_escaped(Iterator begin, Iterator end, Iterator & iter)
 	}
 }
 
+inline void encode_utf8(char32_t code_pt, std::ostringstream & os)
+{
+	if (code_pt < 0x80) {
+		os.put(code_pt & 0xff);
+	} else if (code_pt < 0x8000) {
+		os.put((code_pt >> 6) | 0xc0);
+		os.put((code_pt & 0x3f) | 0x80);
+	} else if (code_pt < 0x1000) {
+		os.put((code_pt >> 12) | 0xe0);
+		os.put(((code_pt >> 6) & 0x3f) | 0x80);
+		os.put((code_pt & 0x3f) | 0x80);
+	} else {
+		os.put((code_pt >> 18) | 0xf0);
+		os.put(((code_pt >> 12) & 0x3f) | 0x80);
+		os.put(((code_pt >> 6) & 0x3f) | 0x80);
+		os.put((code_pt & 0x3f) | 0x80);
+	}
+}
+
 template <class Iterator>
-String parse_string(Iterator begin, Iterator end, Iterator & iter)
+String parse_string(Iterator & begin, Iterator end)
 {
 	long last_code_pt = -1;
 	std::ostringstream oss;
-	consume(begin, end, '"', iter);
-	while (iter != end) {
-		switch (const char chr = *iter) {
+	consume(begin, end, '"');
+	while (begin != end) {
+		switch (const auto chr = *begin) {
 		case '"':
 			if (last_code_pt != -1)
 				encode_utf8(last_code_pt, oss);
-			return ++iter, oss.str();
+			return ++begin, oss.str();
 		case '\\': {
 			char16_t code_pt;
-			if (try_parse_codept(iter, end, iter, code_pt)) {
+			if (try_parse_codept(begin, end, code_pt)) {
 				if (is_codept_group(last_code_pt, code_pt)) {
 					const auto hi = last_code_pt - 0xd800;
 					const auto lo = code_pt - 0xdc00 + 0x10000;
@@ -235,7 +241,7 @@ String parse_string(Iterator begin, Iterator end, Iterator & iter)
 					last_code_pt = code_pt;
 				}
 			} else {
-				const auto esc = parse_escaped(iter, end, iter);
+				const auto esc = parse_escaped(begin, end);
 				if (last_code_pt != -1)
 					encode_utf8(last_code_pt, oss);
 				last_code_pt = -1;
@@ -250,34 +256,34 @@ String parse_string(Iterator begin, Iterator end, Iterator & iter)
 			if (std::iscntrl(chr))
 				throw ParseError("unescaped data in string");
 			oss.put(chr);
-			++iter;
+			++begin;
 		}
 	}
 	throw ParseError("unexpected end of input");
 }
 
 template <class Iterator>
-Array parse_array(Iterator begin, Iterator end, Iterator & iter)
+Array parse_array(Iterator & begin, Iterator end)
 {
 	Array array;
-	consume(begin, end, '[', iter);
+	consume(begin, end, '[');
 	char_type<Iterator> last_token = '[';
-	while (iter != end) {
-		skip_whitespace(iter, end, iter);
-		const auto chr = *iter;
+	while (begin != end) {
+		skip_whitespace(begin, end);
+		const auto chr = *begin;
 		switch (chr) {
 		case ',':
 			if (last_token == '[' || last_token == ',')
 				throw ParseError("unexpected ',' token");
-			++iter; break;
+			++begin; break;
 		case ']':
 			if (last_token == ',')
 				throw ParseError("unexpected ',' token");
-			return ++iter, array;
+			return ++begin, array;
 		default:
 			if (last_token != '[' && last_token != ',')
 				throw ParseError("expected ',' or ']' token");
-			array.emplace_back(parse_value(iter, end, iter));
+			array.emplace_back(parse_value(begin, end));
 		}
 		last_token = chr;
 	}
@@ -285,123 +291,130 @@ Array parse_array(Iterator begin, Iterator end, Iterator & iter)
 }
 
 template <class Iterator>
-KeyValuePair parse_pair(Iterator begin, Iterator end, Iterator & iter)
+KeyValuePair parse_pair(Iterator & begin, Iterator end)
 {
-	const auto key = parse_string(begin, end, iter);
-	skip_whitespace(iter, end, iter);
-	consume(iter, end, ':', iter);
-	skip_whitespace(iter, end, iter);
-	const auto value = parse_value(iter, end, iter);
+	const auto key = parse_string(begin, end);
+	skip_whitespace(begin, end);
+	consume(begin, end, ':');
+	skip_whitespace(begin, end);
+	const auto value = parse_value(begin, end);
 	return std::make_pair(std::move(key), std::move(value));
 }
 
 template <class Iterator>
-Object parse_object(Iterator begin, Iterator end, Iterator & iter)
+Object parse_object(Iterator & begin, Iterator end)
 {
 	Object object;
-	consume(begin, end, '{', iter);
+	consume(begin, end, '{');
 	char_type<Iterator> last_token = '{';
-	while (iter != end) {
-		skip_whitespace(iter, end, iter);
-		const auto chr = *iter;
-		switch (chr) {
+	while (begin != end) {
+		skip_whitespace(begin, end);
+		const auto chr = *begin;
+		switch (*begin) {
 		case ',':
 			if (last_token == '{' || last_token == ',')
 				throw ParseError("unexpected ',' token");
-			++iter; break;
+			++begin; break;
 		case '}':
 			if (last_token == ',')
 				throw ParseError("unexpected ',' token");
-			return ++iter, object;
+			return ++begin, object;
 		default:
 			if (last_token != '{' && last_token != ',')
 				throw ParseError("expected ',' or '}' token");
-			object.emplace(parse_pair(iter, end, iter));
+			object.emplace(parse_pair(begin, end));
 		}
 		last_token = chr;
 	}
 	throw ParseError("unexpected end of input");
 }
 
-template <class Iterator>
-int parse_sign_or(Iterator begin, Iterator end, int defvalue, Iterator & iter)
+template <class Iterator, typename Sign>
+Sign parse_sign_or(Iterator & begin, Iterator end, Sign defvalue)
 {
-	iter = begin;
-	if (iter != end) {
-		switch (*iter) {
-		case '-': ++iter; return -1;
-		case '+': ++iter; return +1;
+	if (begin != end) {
+		switch (*begin) {
+		case '-': ++begin; return -1;
+		case '+': ++begin; return +1;
 		}
 	}
 	return defvalue;
 }
 
 template <class Iterator, typename Num>
-bool try_parse_num(Iterator begin, Iterator end, Iterator & iter, Num & num)
+bool try_parse_num(Iterator & begin, Iterator end, Num & num)
 {
-	num = 0;
-	iter = begin;
-	for (; iter != end; ++iter) {
-		const auto c = *iter;
-		if (!std::isdigit(c))
+	Num result = 0;
+	Iterator try_iter;
+	for (try_iter = begin; try_iter != end; ++try_iter) {
+		const auto chr = *try_iter;
+		if (!std::isdigit(chr))
 			break;
-		num *= 10;
-		num += c - '0';
+		result *= 10;
+		result += chr - '0';
 	}
-	return iter != begin;
+	if (try_iter == begin)
+		return false;
+	begin = try_iter;
+	num = result;
+	return true;
 }
 
 template <class Iterator, typename Frac>
-bool try_parse_frac(Iterator begin, Iterator end, Iterator & iter, Frac & frac)
+bool try_parse_frac(Iterator & begin, Iterator end, Frac & frac)
 {
-	if (!try_consume(begin, end, '.', iter))
+	Iterator try_iter = begin;
+	Frac factor = 0.1, result = 0;
+	if (!try_consume(try_iter, end, '.'))
 		return false;
-	double factor = 0.1;
-	const auto start = iter;
-	for (frac = 0; iter != end; ++iter) {
-		const auto c = *iter;
-		if (!std::isdigit(c))
+	const auto start_iter = try_iter;
+	while (try_iter != end) {
+		const auto chr = *try_iter++;
+		if (!std::isdigit(chr))
 			break;
-		frac += (c - '0') * factor;
+		result += (chr - '0') * factor;
 		factor /= 10;
 	}
-	return iter != start;
+	if (try_iter == start_iter)
+		return false;
+	begin = try_iter;
+	frac = result;
+	return true;
 }
 
 template <class Iterator, typename Exp>
-bool try_parse_exp(Iterator begin, Iterator end, Iterator & iter, Exp & exp)
+bool try_parse_exp(Iterator & begin, Iterator end, Exp & exp)
 {
-	Iterator try_iter;
-	if (!try_consume(begin, end, 'e', try_iter)
-	    && !try_consume(begin, end, 'E', try_iter))
+	Exp num;
+	Iterator try_iter = begin;
+	if (!try_consume(try_iter, end, 'e')
+	    && !try_consume(try_iter, end, 'E'))
 		return false;
-	unsigned int num;
-	const int sig = parse_sign_or(try_iter, end, +1, try_iter);
-	if (!try_parse_num(try_iter, end, try_iter, num))
+	const int sig = parse_sign_or(try_iter, end, +1);
+	if (!try_parse_num(try_iter, end, num))
 		return false;
-	iter = try_iter;
+	begin = try_iter;
 	exp = sig * num;
 	return true;
 }
 
-template <typename CharT>
-constexpr bool is_valid_number_start(CharT chr)
+template <typename Char>
+constexpr bool is_valid_number_start(Char chr)
 {
 	return std::isdigit(chr) || chr == '-' || chr == '.';
 }
 
 template <class Iterator>
-Value parse_number(Iterator begin, Iterator end, Iterator & iter)
+Value parse_number(Iterator & begin, Iterator end)
 {
-	iter = begin;
 	Int dec = 0, exp = 0; Real frac = 0;
-	if (!is_valid_number_start(peek_char(iter, end)))
+	if (!is_valid_number_start(peek_char(begin, end)))
 		throw ParseError("invalid value");
-	const long sig = parse_sign_or(iter, end, +1, iter);
-	const auto dec_start = peek_char(iter, end);
-	const bool has_dec = try_parse_num(iter, end, iter, dec);
-	const bool has_frac = try_parse_frac(iter, end, iter, frac);
-	const bool has_exp = try_parse_exp(iter, end, iter, exp);
+	const long sig = parse_sign_or(begin, end, +1);
+	const auto dec_start = peek_char(begin, end);
+	const bool has_dec = try_parse_num(begin, end, dec);
+	const bool has_frac = try_parse_frac(begin, end, frac);
+	const bool has_exp = try_parse_exp(begin, end, exp);
 	const bool is_real = has_frac || has_exp;
 	if (!is_real) {
 		if (!has_dec)
@@ -414,19 +427,17 @@ Value parse_number(Iterator begin, Iterator end, Iterator & iter)
 }
 
 template <class Iterator>
-Value parse_value(Iterator begin, Iterator end, Iterator & iter)
+Value parse_value(Iterator & begin, Iterator end)
 {
-	skip_whitespace(begin, end, iter);
-	if (iter == end)
-		throw ParseError("unexpected end of input");
-	switch (*begin) {
-	case 'n': return parse_null(iter, end, iter);
-	case 't': return parse_true(iter, end, iter);
-	case 'f': return parse_false(iter, end, iter);
-	case '"': return parse_string(iter, end, iter);
-	case '[': return parse_array(iter, end, iter);
-	case '{': return parse_object(iter, end, iter);
-	default:  return parse_number(iter, end, iter);
+	skip_whitespace(begin, end);
+	switch (peek_char(begin, end)) {
+	case 'n': return parse_null(begin, end);
+	case 't': return parse_true(begin, end);
+	case 'f': return parse_false(begin, end);
+	case '"': return parse_string(begin, end);
+	case '[': return parse_array(begin, end);
+	case '{': return parse_object(begin, end);
+	default:  return parse_number(begin, end);
 	}
 }
 
@@ -435,14 +446,14 @@ Value parse_value(Iterator begin, Iterator end, Iterator & iter)
 template <class Iterator>
 Value parse(Iterator begin, Iterator end)
 {
-	return detail::parse_value(begin, end, begin);
+	return detail::parse_value(begin, end);
 }
 
-template <typename CharT>
-Value parse(std::basic_istream<CharT> & is)
+template <typename Char>
+Value parse(std::basic_istream<Char> & is)
 {
-	return parse(std::istream_iterator<CharT>(is),
-	             std::istream_iterator<CharT>());
+	return parse(std::istream_iterator<Char>(is),
+	             std::istream_iterator<Char>());
 }
 
 }
--- src/parse.cpp
+++ src/parse.cpp
@@ -22,27 +22,4 @@ Value parse(detail::u32string_view sv)
 	return parse(sv.begin(), sv.end());
 }
 
-namespace detail {
-
-void encode_utf8(char32_t code_pt, std::ostringstream & os)
-{
-	if (code_pt < 0x80) {
-		os.put(code_pt & 0xff);
-	} else if (code_pt < 0x8000) {
-		os.put((code_pt >> 6) | 0xc0);
-		os.put((code_pt & 0x3f) | 0x80);
-	} else if (code_pt < 0x1000) {
-		os.put((code_pt >> 12) | 0xe0);
-		os.put(((code_pt >> 6) & 0x3f) | 0x80);
-		os.put((code_pt & 0x3f) | 0x80);
-	} else {
-		os.put((code_pt >> 18) | 0xf0);
-		os.put(((code_pt >> 12) & 0x3f) | 0x80);
-		os.put(((code_pt >> 6) & 0x3f) | 0x80);
-		os.put((code_pt & 0x3f) | 0x80);
-	}
-}
-
-}
-
 }
